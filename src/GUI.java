7eeeb37ed66b3300b75aa9ce6cdb6089ac133083
Joshua Thrower <joshuareplacement@gmail.com>
2024-02-29 15:25:11 +0000

    Logout button work & code readability

--- src/GUI.java
+++ src/GUI.java
@@ -20,68 +20,7 @@ public class GUI {
         ui.loginInterface();
     }
 
-
-    /**
-     * The profile section of the project.
-     *
-     * @param username The username of the current user.
-     * @author max
-     */
-    private void openProfile(String username, String password) {
-        frame = new JFrame("Profile");
-        frame.setSize(600, 700);
-        frame.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        DBManager database = new DBManager();
-        HashMap<String, Object> userInformation = database.getUserInfo(username, password);
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-
-        JLabel welcomeMessage = new JLabel("Hello, " + userInformation.get("name"));
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.anchor = GridBagConstraints.NORTHWEST;
-        frame.add(welcomeMessage, gbc);
-
-        //messagelist
-        //TODO: i guess the messages are gonna be stored on the db so we need them from the db
-        String[] messages = {"Message 1", "Message 2", "Message 3"};
-        JList<String> messageList = new JList<>(messages);
-        JScrollPane scrollPane = new JScrollPane(messageList);
-        scrollPane.setPreferredSize(new Dimension(580, 200));
-        scrollPane.setBorder(BorderFactory.createTitledBorder("Messages"));
-        panel.add(scrollPane, BorderLayout.CENTER);
-
-        //panel
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.anchor = GridBagConstraints.NORTHWEST;
-        //gbc.fill = GridBagConstraints.BOTH;
-        gbc.weightx = 1.0;
-        gbc.weighty = 1.0;
-
-        panel.setBorder(BorderFactory.createLineBorder(Color.RED));
-        frame.add(panel, gbc);
-
-        //logout button
-        gbc.gridy++;
-        gbc.anchor = GridBagConstraints.CENTER;
-        gbc.fill = GridBagConstraints.NONE;
-        JButton logoutButton = new JButton("Logout");
-        frame.add(logoutButton, gbc);
-
-        //logout button
-        logoutButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-
-            }
-        });
-
-        frame.setVisible(true);
-    }
+//---------------------------------------------------------------------------------------------------------------------------------------------
 
     /**
      * The login part of the project.
@@ -91,8 +30,8 @@ public class GUI {
     private void loginInterface() {
         frame = new JFrame("Login");
 
-        //TODO: Think about changing the size of the window.
-        //TODO: Also think about changing the look of the whole thing because it's ugly. Doesn't give extra marks.
+        // TODO: Think about changing the size of the window.
+        // TODO: Also think about changing the look of the whole thing because it's ugly. Doesn't give extra marks.
         frame.setSize(400, 300);
         frame.setLayout(new GridBagLayout());
 
@@ -149,4 +88,70 @@ public class GUI {
 
         frame.setVisible(true);
     }
+
+//---------------------------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * The profile section of the project.
+     *
+     * @param username The username of the current user.
+     * @author max
+     */
+    private void openProfile(String username, String password) {
+        frame = new JFrame("Profile");
+        frame.setSize(600, 700);
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        DBManager database = new DBManager();
+        HashMap<String, Object> userInformation = database.getUserInfo(username, password);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        JLabel welcomeMessage = new JLabel("Hello, " + userInformation.get("name"));
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        frame.add(welcomeMessage, gbc);
+
+        // messagelist
+        // TODO: I guess the messages are gonna be stored on the db so we need them from the db
+        String[] messages = {"Message 1", "Message 2", "Message 3"};
+        JList<String> messageList = new JList<>(messages);
+        JScrollPane scrollPane = new JScrollPane(messageList);
+        scrollPane.setPreferredSize(new Dimension(580, 200));
+        scrollPane.setBorder(BorderFactory.createTitledBorder("Messages"));
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        //panel
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        //gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+
+        panel.setBorder(BorderFactory.createLineBorder(Color.RED));
+        frame.add(panel, gbc);
+
+        //logout button
+        gbc.gridy++;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.fill = GridBagConstraints.NONE;
+        JButton logoutButton = new JButton("Logout");
+        frame.add(logoutButton, gbc);
+
+        //logout button
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                loginInterface();
+            }
+        });
+
+        frame.setVisible(true);
+    }
+
+    
 }
